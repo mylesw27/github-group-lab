29c15bb5c72ad70aa4233dcbb005a47983a28404
juanedcabrera <juanedcabrera@gmail.com>
2023-05-01 16:30:38 -0600

    Initial about page

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about">
+      <h2>Name</h2>
+      <p>Phrase</p>
+      <img src="image-url" alt="alt-text" />
+      <h3>Fun Facts</h3>
+      <ul>
+        <li>Fact 1</li>
+        <li>Fact 2</li>
+        <li>Fact 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default About;
