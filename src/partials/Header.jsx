484f52d5dea377a667958042bda5e053e3eb8ae4
awellsbiz <awellsbiz@gmail.com>
2023-05-01 15:14:44 -0700

    Header component stubbed out in partials

---
+++ src/partials/Header.jsx
@@ -0,0 +1,17 @@
+import {Link} from 'react-router-dom'
+
+//For the header navigation I want to have 2-3 links that will navigate to the different pages for the app. 
+//include the logo inside of the navigation or a search bar.
+//maybe you can create the logo above the navigation if you want to get fancy. 
+
+//first stub out with nav tags.
+//then go into li- make sure that they are displayed in line with good padding. 
+
+export default function Header(){
+    return(
+        <div>
+            Hello, from Anthony! The Nav master.
+        
+        </div>
+    )
+}
